0aba60f1229f9856d98ddcc5e4ff69ae235d8289
Sajjad Habibi <sajjadhabibi.1006@gmail.com>
2026-08-25 00:45:52 +0330

    Add Navigation examples

---
+++ Navigation/Example 11-NavigationNavigate in Screens-JetpackCompose-MainActivity.kt
@@ -0,0 +1,12 @@
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                val navController = rememberNavController()
+                Nav(navController)
+
+            }
+        }
+    }
+}
---
+++ Navigation/Example 11-ScreenC-JetpackCompose.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@Composable
+fun ScreenC(navController: NavHostController){
+    Column(modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Text(text = "Screen C", fontSize = 65.sp)
+        Spacer(modifier = Modifier.height(45.dp))
+        Button(onClick = {
+            navController.navigate("A")
+            {popUpTo("A"){inclusive = true} }
+        }) {
+            Text(text = "Go to screen A", fontSize = 40.sp)
+        }
+    }
+}
